7e0c7c633e6535f5b09068d3a3acc3e8d7efc978
koda-git <85606515+koda-git@users.noreply.github.com>
2024-09-18 15:33:47 -0400

    Fixed navbar linking and cleaned code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,18 +11,13 @@ const NavBar = () => {
                 <div className="menu hidden md:block md:w-auto" id="navbar">
                     <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 ">
                         <li className="nav-item">
-                            <Link href="/home" passHref>
+                            <Link href="/" passHref>
                                 <span className="text-white text-lg md:text-xl font-semibold">Home</span>
                             </Link>
                         </li>
                         <li className="nav-item">
                             <Link href="/about" passHref>
                                 <span className="text-white text-lg md:text-xl font-semibold">About</span>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link href="/services" passHref>
-                                <span className="text-white text-lg md:text-xl font-semibold">Services</span>
                             </Link>
                         </li>
                         <li className="nav-item">
